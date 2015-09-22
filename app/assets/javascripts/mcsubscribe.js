b46804031302f2d32cd77c003fc0e98ce2326f27
Justin Tanacredi <jtanacredi@gmail.com>
2015-09-22 19:53:03 -0400

    Background - add webgl drop effect

--- app/assets/javascripts/mcsubscribe.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function() {
-// 	$(function() {
-//     var BV = new $.BigVideo();
-//     BV.init();
-//     BV.show('http://vjs.zencdn.net/v/oceans.mp4',{ambient:true});
-// });
-// });
